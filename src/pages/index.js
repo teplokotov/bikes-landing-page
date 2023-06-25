a1575c4526c87b053f5f4a32a3bf6c52a96a949b
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-06-25 18:53:40 +0300

    feat: add mobile-menu

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,10 @@
 // Styles
 import './index.scss';
+
+const burgerMenu = document.querySelector('.burger-menu');
+const headerNav = document.querySelector('.header__nav');
+
+burgerMenu.addEventListener('click', (evt) => {
+  burgerMenu.classList.toggle('burger-menu_opened');
+  headerNav.classList.toggle('header__nav_opened');
+});
