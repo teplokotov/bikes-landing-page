c70b719de7ba2d2f22941c0058d180671dab944d
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-06-30 01:43:48 +0300

    feat: add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(ico|png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
       },
       {
